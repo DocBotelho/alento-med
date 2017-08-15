8ceb14956523c0dbebef17c625f7bfe950bb3336
Claudio Botelho <claudio.botelho@smartificar.com.br>
2017-08-15 15:04:28 -0300

    Added name and phone to User model. Updated Sign Up fields to grab these info.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170814193329) do
+ActiveRecord::Schema.define(version: 20170815175717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,6 +86,8 @@ ActiveRecord::Schema.define(version: 20170814193329) do
     t.string   "location"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "phone"
+    t.string   "name"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
