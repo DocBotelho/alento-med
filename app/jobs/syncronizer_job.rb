011240e83790cf849f3736fa9074dc3f3938fe7c
Victor Wilm <victorrw@msn.com>
2017-08-16 11:23:20 -0300

    Commenting unfinished job

--- app/jobs/syncronizer_job.rb
+++ app/jobs/syncronizer_job.rb
@@ -2,12 +2,14 @@ class SyncronizerJob < ApplicationJob
   queue_as :default
 
   def perform
-    Study.joins("INNER JOIN facilities ON facilities.nct_id = studies.nct_id INNER JOIN facility_investigators ON facility_investigators.facility_id = facilities.id INNER JOIN facility_contacts ON facility_contacts.facility_id = facilities.id INNER JOIN conditions ON conditions.nct_id = studies.nct_id INNER JOIN calculated_values ON calculated_values.nct_id = studies.nct_id INNER JOIN designs ON designs.nct_id = studies.nct_id INNER JOIN eligibilities ON eligibilities.nct_id = studies.nct_id").where("facilities.country = 'Brazil' AND facilities.status = 'Recruiting' AND study_type = 'Interventional'").select('*')find_each do |f|
+    puts "================ I'm working! ===================="
+    # Study.joins("INNER JOIN facilities ON facilities.nct_id = studies.nct_id INNER JOIN facility_investigators ON facility_investigators.facility_id = facilities.id INNER JOIN facility_contacts ON facility_contacts.facility_id = facilities.id INNER JOIN conditions ON conditions.nct_id = studies.nct_id INNER JOIN calculated_values ON calculated_values.nct_id = studies.nct_id INNER JOIN designs ON designs.nct_id = studies.nct_id INNER JOIN eligibilities ON eligibilities.nct_id = studies.nct_id").where("facilities.country = 'Brazil' AND facilities.status = 'Recruiting' AND study_type = 'Interventional'").select('*').find_each do |f|
 
-      @trial = Trial.new(title: f.brief_title, condition: f.name, eligibility: f.criteria, description: f.description)
-      @trial.save
+    #   @trial = Trial.new(title: f.brief_title, condition: f.name, eligibility: f.criteria, description: f.description)
+    #   @trial.save
 
-      @institution.new(name:)
+    #   @institution.new(name:)
+    # end
 
 
     #.joins('INNER JOIN participant_flows on participant_flows.nct_id = facilities.nct_id').joins('INNER JOIN result_contacts on result_contacts.nct_id = facilities.nct_id').joins('INNER JOIN result_contacts on result_contacts.nct_id = facilities.nct_id').find_each do |f|
