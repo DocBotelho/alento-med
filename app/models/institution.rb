270276f4c8b28394e2815c44645428498f62cbc8
Claudio Botelho <claudio.botelho@smartificar.com.br>
2017-08-16 18:58:00 -0300

    models revised to allow combined search

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -1,4 +1,10 @@
 class Institution < ApplicationRecord
+  # Added to allow search through treatment model
+  has_many :treatments
+  # Added to allow combined search through trialinstitution model
+  has_many :trialistitutions
+  # Added to allow combined search through trialinstitution model
+  has_many :trials, through: :trialistitutions
   # Added for geocoding
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
--- app/models/trial.rb
+++ app/models/trial.rb
@@ -1,2 +1,12 @@
 class Trial < ApplicationRecord
+  # Added to allow search through treatment model
+  has_many :treatments
+  # Added to allow combined search through trialinstitution model
+  has_many :trialinstitutions
+  # Added to allow combined search through trialinstitution model
+  has_many :institutions, through: :trialinstitutions
+  # Added to allow combined search through trialdoctor model
+  has_many :trialdoctors
+  # Added to allow combined search through trialdoctor model
+  has_many :doctors, through: :trialdoctors
 end
