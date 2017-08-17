8061fdc39b551586b71cf5cca117b33e1d933d9d
Claudio Botelho <claudio.botelho@smartificar.com.br>
2017-08-17 16:09:58 -0300

    swapped treatments controller/view#show to institutions

--- app/controllers/treatments_controller.rb
+++ app/controllers/treatments_controller.rb
@@ -7,23 +7,7 @@ class TreatmentsController < ApplicationController
 
 
   def index
-    # Added to find locations where the medical condition informed on the search bar is treated
-    @condition = params[:condition]
-    @trials = Trial.find_by(condition: @condition)
-    if @trials.nil?
-      # Add message on screen "SORRY, NO TREATMENTS AVAILABLE AT THIS MOMENT TO THE CONDITION YOU SEARCHED"
-      @institutions = []
-    else
-      @institutions = @trials.institutions
-      # Added for geocoding. MUST CHANGE @treatments to receive Treatment.where.not instead
-      @institutions = @institutions.where.not(latitude: nil, longitude: nil)
-    end
-
-    @hash = Gmaps4rails.build_markers(@institutions) do |institution, marker|
-      marker.lat institution.latitude
-      marker.lng institution.longitude
-      # marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
-    end
+    #MOVED TO INSTITUTION CONTROLLER By DocBotelho
   end
 
   def show
