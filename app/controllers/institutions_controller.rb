ac349f4a134d34069f7f42778476265ffab944b3
Claudio Botelho <claudio.botelho@smartificar.com.br>
2017-08-22 18:43:37 -0300

    forced institution controller to run app

--- app/controllers/institutions_controller.rb
+++ app/controllers/institutions_controller.rb
@@ -1,12 +1,16 @@
 class InstitutionsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
+  # CONTROLLER ALTERED BY DOCBOTELHO TO FORCE RUNNING BEFORE INSTALLING PGSEARCH GEM
+
   def index
     @condition = params[:condition]
 
     # review line to fix search by distance from user_location
-    @institutions = Institution.joins(:trials).where('trials.condition @@ ?', "#{@condition}").where.not(latitude: nil, longitude: nil).distinct
+    @institutions = Institution.where('address @@ ?', 'São Paulo')
 
+    # commmented line below and replaced by line above to make it run before pgsearch
+    # Institution.joins(:trials).where('trials.condition @@ ?', "#{@condition}").where.not(latitude: nil, longitude: nil).distinct
     # .page(params[:page])
 
     @hash = Gmaps4rails.build_markers(@institutions) do |institution, marker|
@@ -16,9 +20,16 @@ class InstitutionsController < ApplicationController
   end
 
   def show
-    @institution = Institution.find(params[:id])
-    @condition = params[:condition]
-    @trials = @institution.trials.where('condition @@ ?', "#{@condition}")
+    @institution = Institution.find(1)
+    # commmented line below and replaced by line above to make it run before pgsearch
+    # @institution = Institution.find(params[:id])
+
+    @condition = "diabetes virtual do tipo ZZZ"
+
+
+    @trials = Trial.where('title @@ ?', 'study')
+    # commmented line below and replaced by line above to make it run before pgsearch
+    # @trials = @institution.trials.where('condition @@ ?', "#{@condition}")
   end
 
   private
