1072010a2068e569bdc58571d2d995dd9428bb6c
Victor Wilm <victorrw@msn.com>
2017-08-25 00:21:55 -0300

    Bigger descriptions

--- app/services/syncer.rb
+++ app/services/syncer.rb
@@ -32,12 +32,24 @@ class Syncer
       end
     end
 
-    Study.joins("INNER JOIN detailed_descriptions on detailed_descriptions.nct_id = studies.nct_id INNER JOIN facilities on facilities.nct_id = studies.nct_id").where(WHERE).select("detailed_descriptions.description as description, detailed_descriptions.nct_id as description_nct_id").each do |desc|
-      trial = Trial.find_by(trial_nct_id: desc.description_nct_id)
-      trial.description = desc.description
-      trial.save!
-      puts "Added description to trial"
-    end
+    Study.joins("INNER JOIN detailed_descriptions ON detailed_descriptions.nct_id = studies.nct_id INNER JOIN facilities ON facilities.nct_id = studies.nct_id").where(WHERE).select("detailed_descriptions.description as description, detailed_descriptions.nct_id as description_nct_id").each do |desc|
+        Trial.where(trial_nct_id: desc.description_nct_id).update(description: desc.description)
+          # trial = Trial.find_by(trial_nct_id: desc.description_nct_id)
+          #   trial.description = desc.description
+          #   trial.save!
+          puts "Added description to trial"
+      end
+
+      Study.joins("INNER JOIN facilities on facilities.nct_id = studies.nct_id INNER JOIN brief_summaries on brief_summaries.nct_id = studies.nct_id").where(WHERE).select("brief_summaries.nct_id as brief_summaries_nct_id, brief_summaries.description as brief_summaries_description").each do |desc|
+       Trial.where(trial_nct_id: desc.brief_summaries_nct_id, description: nil).update(description: desc.brief_summaries_description)
+        puts "Added description to nil"
+     end
+
+      # if trial.description == nil
+      #   trial.description = desc.intervention_description if trial.trial_nct_id == desc.interventions_nct_id
+      # end
+      # if trial.description == nil
+      #   trial.description = desc.design_groups_description if trial.trial_nct_id == desc.design_groups_nct_id
 
     Study.joins("INNER JOIN eligibilities on eligibilities.nct_id = studies.nct_id INNER JOIN facilities on facilities.nct_id = studies.nct_id").where(WHERE).select("eligibilities.gender as gender, eligibilities.maximum_age as maximum_age, eligibilities.minimum_age as minimum_age, eligibilities.nct_id as eligibility_nct_id").each do |eli|
       trial = Trial.find_by(trial_nct_id: eli.eligibility_nct_id)
