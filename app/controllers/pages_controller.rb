fcbf91ebc768b8e3cc5dba67089a577d46c7541a
Claudio Botelho <claudio.botelho@smartificar.com.br>
2018-05-29 01:43:31 -0300

    removed oauth from privacypolicies page on pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,11 @@
 class PagesController < ApplicationController
   # Added to skip authentication on home/root page
-  skip_before_action :authenticate_user!, only: [:home, :sobrenos]
+  skip_before_action :authenticate_user!, only: [:home, :sobrenos, :privacypolicies]
 
   def home
+  end
+
+  def sobrenos
   end
 
   def sobrenos
