53961f1b5ca70a2fafd380721fff286ad9690342
Claudio Botelho <claudio.botelho@smartificar.com.br>
2018-05-29 01:35:26 -0300

    pull request new attempt

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,11 @@
 class PagesController < ApplicationController
   # Added to skip authentication on home/root page
-  skip_before_action :authenticate_user!, only: [:home, :sobrenos :privacypolicies]
+  skip_before_action :authenticate_user!, only: [:home, :sobrenos]
 
   def home
   end
 
   def sobrenos
-  end
-
-  def privacypolicies
   end
 
 end
